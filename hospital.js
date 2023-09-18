1310874bfd00e100122fcaf6ea33c0474ee8caa0
zhoulifei8848 <144153894+zhoulifei8848@users.noreply.github.com>
2023-09-17 21:22:34 -0400

    Update hospital.js

--- hospital.js
+++ hospital.js
@@ -1,6 +1,3 @@
-// Start a new git project. Make sure to commit and sync your changes after every requirement is complete, 
-//The commit message should contain the requirement(s) that was addressed in that commit.
-
 // Part 1:
 // Create a data structure that describes a "hospital". It has a name and contains 3 "patient" records. 
 //Each "patient" has an id, fullName, dateOfBirth, and multiple "symptoms". Each symptom in a String. 
